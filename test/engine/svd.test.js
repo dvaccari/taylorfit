126a2ddfac98103dddbdc1a4adee37178c3dbc4d
patgrasso <pgrasso@stevens.edu>
2017-01-31 12:25:08 -0500

    Create tests for the SVD algorithm itself
    Uses test cases used for the original implementation, stored in
    svd.data.json.

---
+++ test/engine/svd.test.js
@@ -0,0 +1,43 @@
+/*global describe, it, before*/
+
+const chai    = require('chai')
+    , expect  = chai.expect
+    , should  = chai.should;
+
+const Matrix  = require('../../engine/matrix').Matrix
+    , svd     = require('../../engine/matrix').svd
+    , dataset = require('./svd.data.json');
+
+describe('svd', () => {
+
+  var data;
+
+  before(() => {
+    data = {
+      A: new Matrix(dataset.A),
+      w: dataset.w
+    };
+  });
+
+  it('computes the proper singular values of a matrix', () => {
+    var singularVals = svd(data.A)[1]
+      , i;
+
+    // Check each singular value for approximate equality (+/- 1e-12)
+    for (i = 0; i < singularVals.length; i += 1) {
+      expect(singularVals[i]).to.be.approximately(data.w[i], 1e-12);
+    }
+  });
+
+  it('svd(A) -> U,S,V s.t. A = U * diag(S) * V.T', () => {
+    var [u, s, v] = svd(data.A)
+      , usv = u.dot(Matrix.diag(s)).dot(v.T)
+      , i;
+
+    // Check each element of A with USV for approximate equality (+/- 1e-12)
+    for (i = 0; i < data.A.data.length; i += 1) {
+      expect(usv.data[i]).to.be.approximately(data.A.data[i], 1e-12);
+    }
+  });
+
+});
